f40c7cfdae10094714e98ec3687663a7f09b366d
boostedgagi <dragan.02jelic@gmail.com>
2022-07-07 13:05:36 +0200

    For some reason edit reservation component was not included.

---
+++ src/app/components/edit-reservation/edit-reservation.component.ts
@@ -0,0 +1,30 @@
+import {Component, EventEmitter, OnInit,Input,Output} from '@angular/core';
+import {Reservation} from "../../Reservation";
+import {ModalService} from "../../services/modal.service";
+
+
+@Component({
+  selector: 'app-edit-reservation',
+  templateUrl: './edit-reservation.component.html',
+  styleUrls: ['./edit-reservation.component.css']
+})
+export class EditReservationComponent implements OnInit {
+  @Input() reservation?: Reservation;
+  @Output() onUpdateReservation:EventEmitter<Reservation> = new EventEmitter<Reservation>();
+  @Output() close:EventEmitter<boolean> = new EventEmitter<boolean>();
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  updateReservation(reservation?: Reservation) {
+    this.onUpdateReservation.emit(reservation);
+  }
+
+  onClose(){
+    this.close.emit(true);
+  }
+
+}
