bede3efb746f91502988824f946be0d2987b6d4e
boostedgagi <dragan.02jelic@gmail.com>
2022-07-11 15:55:07 +0200

    Everything merged, we have insert, delete and hopefully edit for rooms and reservations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {FormsModule} from '@angular/forms';
 import {Router, RouterModule, Routes} from "@angular/router";
 import { MatDialogModule } from '@angular/material/dialog';
 
